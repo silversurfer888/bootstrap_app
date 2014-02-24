613843c2a5c5fb14c0100b4985938cf117a49eed
Peter <cpeteryu@gmail.com>
2014-02-24 01:18:21 -0500

    Create Setup and Bootstrap gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapApp::Application.config.secret_key_base = '8fb772cde6f7fa9ec0779977e6f8df64c84c5d30bdfe3156537cbc162f2e02437da84ae57a456886e1b1c0c3ae7ff0345720caa2ff777b4005c7c8c6265bcb02'
